68003e78ff45539980852c53c2068178839e75ec
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-06-30 07:18:53 +0300

    edit

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 #  using Puppet to make changes to our configuration file.
 file_line { 'identity file for private key':
     ensure => 'present',
-    path   => '~/.ssh/config',
+    path   => '/etc/ssh/ssh_config',
     line   => '    IdentityFile ~/.ssh/school',
 }
 
 file_line { 'turn off passwd authentication':
     ensure => 'present',
-    path   => '~/.ssh/config',
+    path   => '/etc/ssh/ssh_config',
     line   => '    PasswordAuthentication no',
 }
