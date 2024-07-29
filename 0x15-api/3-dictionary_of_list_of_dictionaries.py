ed9e7fa26fcad0e06bf7c732aa1a0fc1d0985dc8
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-07-30 02:52:44 +0300

    edit

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+'''Module to return information about TODO list progress.'''
+import json
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/users"
+    users = requests.get(url).json()
+    
+    # Dictionary to store user data and their tasks
+    data = {}
+    
+    for user in users:
+        userId = user.get('id')
+        username = user.get('username')
+        
+        # Fetch tasks for this user
+        url_tasks = f"https://jsonplaceholder.typicode.com/todos?userId={userId}"
+        all_tasks = requests.get(url_tasks).json()
+        
+        tasks = []
+        for task in all_tasks:
+            # Modify task dictionary
+            task_data = {
+                'username': username,
+                'task': task.get('title'),
+                'completed': task.get('completed'),
+            }
+            tasks.append(task_data)
+        
+        # Add tasks to the data dictionary for this user
+        data[userId] = tasks
+
+    # Write the data to a JSON file
+    json_file = 'todo_all_employees.json'
+    with open(json_file, 'w') as f:
+        json.dump(data, f)
