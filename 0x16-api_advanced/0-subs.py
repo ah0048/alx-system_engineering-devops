32bda7627261728dfc82861271d0243c5bbd1c03
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-08-13 21:36:15 +0300

    edit 1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,7 +5,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     '''the function'''
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    url = 'https://api.reddit.com/r/{}/about'.format(subreddit)
     headers = {'User-agent': 'alx api advanced task'}
     resp = requests.get(url=url, headers=headers, allow_redirects=False)
     if resp.status_code == 200:
