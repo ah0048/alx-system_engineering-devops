3906fbed8d2c980bfe3a461865d588d32cc5052e
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-08-13 21:40:28 +0300

    edit 2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,24 @@
 #!/usr/bin/python3
-'''a function to return  the number of subscribers'''
+"""
+queries the Reddit API and returns the number
+of subscribers (not active users, total subscribers)
+for a given subreddit. If an invalid subreddit is given,
+the function should return 0.
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    '''the function'''
-    url = 'https://api.reddit.com/r/{}/about'.format(subreddit)
-    headers = {'User-agent': 'alx api advanced task'}
-    resp = requests.get(url=url, headers=headers, allow_redirects=False)
-    if resp.status_code == 200:
-        data = resp.json()
-        return (data['data']['subscribers'])
+    """returns number of total subscribers"""
+    url = ("https://api.reddit.com/r/{}/about".format(subreddit))
+    headers = {'User-Agent': 'CustomClient/1.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        return (0)
+    response = response.json()
+    if 'data' in response:
+        return (response.get('data').get('subscribers'))
+
     else:
-        return 0
+        return (0)
