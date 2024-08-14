372e354ba593bc4e94aba1bfd61a698477d5b73a
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-08-14 18:07:07 +0300

    added comment 4

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # puppet code to fix web server bug
 
-exec { 'fix phpp typo'
+exec { 'fix phpp typo':
     provider => 'shell',
     command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
